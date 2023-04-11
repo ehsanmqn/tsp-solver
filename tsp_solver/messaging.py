68a7163bef524619f6b9bc931897b86f333a1e2e
Ehsan <spin313@gmail.com>
2023-04-11 22:33:45 +0330

    The response message format added as TspResponse class

--- tsp_solver/messaging.py
+++ tsp_solver/messaging.py
@@ -1,9 +1,18 @@
-
 class TspRequest:
     """
     The request message format
     """
+
     def __init__(self, message_id, locations):
         self.message_id = message_id
         self.locations = locations
 
+
+class TspResponse:
+    """
+    The response message format
+    """
+    def __init__(self, message_id, solution, distance):
+        self.message_id = message_id
+        self.solution = solution
+        self.distance = distance
