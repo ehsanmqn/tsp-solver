ff686bf273829a5dd6ef81dfec3406ac0e68bbf8
Ehsan <spin313@gmail.com>
2023-04-14 21:36:16 +0330

    main.py added again to not changing working directory in docker

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+import logging
+
+from tsp_solver import messaging
+
+# Configure logging settings
+logging.basicConfig(filename='../tsp_solver.log', level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(message)s')
+
+
+def main():
+    logging.info('TSP solver service is going to be started.')
+    messaging.start_service()
+
+
+if __name__ == '__main__':
+    sys.exit(main())
+
+# Shutdown the logger when done
+logging.shutdown()
