5d0e9204b08244832b4267bbf4f541e6418b3611
Ehsan <spin313@gmail.com>
2023-04-14 11:55:08 +0330

    Mathematical related functions moved to utils module

--- tsp_solver/messaging.py
+++ tsp_solver/messaging.py
@@ -5,8 +5,7 @@ import os
 import pika
 
 from tsp_solver.vrp_solver import ortools_vrp_solver
-
-scale_factor = 100
+from tsp_solver.utils import euclidean_distance, generate_distances
 
 
 class TspRequest:
@@ -31,23 +30,6 @@ class TspResponse:
         self.solution = solution
         self.code = code
         self.message = message
-
-
-def euclidean_distance(p, q):
-    """
-    Giving points p, and q, this function calculate the Euclidean distance between p, and q
-    :param p: Location 1
-    :param q: Location 2
-    :return: Distance between p, and q
-    """
-    return int(math.sqrt((p['latitude'] - q['latitude']) ** 2 + (p['longitude'] - q['longitude']) ** 2) * scale_factor)
-
-
-def generate_distances(request):
-    distances = [[euclidean_distance(request.locations[i], request.locations[j]) for j in range(len(request.locations))]
-                 for i in range(len(request.locations))]
-
-    return distances
 
 
 def process_message(channel, method, properties, body):
